af998747d15226b7f66f4c5f49fdbbb418d9c450
Till Pinke <t.e.pinke96@gmail.com>
2022-01-02 18:14:26 +0100

    Remove unnecessary dependencies

--- src/error.rs
+++ src/error.rs
@@ -2,13 +2,11 @@ use std::{error, fmt};
 #[derive(Debug)]
 pub enum Error {
     ReqwestError(reqwest::Error),
-    SerdeError(serde_json::Error),
     ChronoParseError(chrono::ParseError),
     ParsingBytesError(String, &'static str),
     UnexpectedResponse(String, &'static str),
     InvalidCharacterCode(&'static str),
     InvalidArgument(String),
-    JsonParsingError(serde_json::Value),
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
@@ -17,7 +15,6 @@ impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Error::ReqwestError(e) => write!(f, "Error making request: {}", e),
-            Error::SerdeError(e) => write!(f, "Error parsing data: {}", e),
             Error::ChronoParseError(e) => write!(f, "Error parsing datetime: {}", e),
             Error::ParsingBytesError(bytes, msg) => write!(f, "{}: {}", msg, bytes),
             Error::UnexpectedResponse(bytes, msg) => {
@@ -25,7 +22,6 @@ impl fmt::Display for Error {
             }
             Error::InvalidCharacterCode(code) => write!(f, "{} is not valid character code", code),
             Error::InvalidArgument(msg) => write!(f, "Invalid argument: {}", msg),
-            Error::JsonParsingError(v) => write!(f, "Could not parse json value: {}", v),
         }
     }
 }
@@ -33,12 +29,6 @@ impl fmt::Display for Error {
 impl From<reqwest::Error> for Error {
     fn from(e: reqwest::Error) -> Self {
         Error::ReqwestError(e)
-    }
-}
-
-impl From<serde_json::Error> for Error {
-    fn from(e: serde_json::Error) -> Self {
-        Error::SerdeError(e)
     }
 }
 
